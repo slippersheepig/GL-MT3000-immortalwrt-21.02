ef22f822a3e29a63c9f40810450698753ad6b96b
sheepgreen <58287293+slippersheepig@users.noreply.github.com>
2025-12-25 19:34:59 +0800

    revert
    Removed the fix for microsocks C99 build error and the associated patch creation.

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,30 +12,3 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
-
-echo "Fix microsocks C99 build error"
-
-PATCH_DIR="package/luci-app-passwall-packages/microsocks/patches"
-mkdir -p $PATCH_DIR
-
-cat > $PATCH_DIR/001-fix-c99-label-declaration.patch << 'EOF'
---- a/sockssrv.c
-+++ b/sockssrv.c
-@@ -499,7 +499,9 @@ handshake(int sock, struct sockaddr *sa)
- 
- error:
--    int used_rule = 0;
-+{
-+    int used_rule = 0;
-+
-     if (acl)
-         used_rule = acl->number;
- 
-@@ -513,6 +515,7 @@ error:
-     if (rules)
-         ruleslog(used_rule, addr, port, username, 0);
- 
-+}
-     close(sock);
-     return -1;
-EOF

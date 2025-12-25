57e869ee2f2de483eda0d9a2dd327f8ddcabeac5
sheepgreen <58287293+slippersheepig@users.noreply.github.com>
2025-12-25 20:55:26 +0800

    Implement revert for bad microsocks commit
    Add script to revert bad commit in microsocks repo

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,23 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
+
+echo "==> Revert microsocks socks5 forwarding rules commit"
+
+MICROSOCKS_DIR="package/feeds/passwall/microsocks"
+BAD_COMMIT="ee784243479b7f10cf197c0246cddaa456a0801d"
+
+if [ -d "$MICROSOCKS_DIR/.git" ]; then
+    cd "$MICROSOCKS_DIR"
+
+    if git cat-file -e ${BAD_COMMIT}^{commit} 2>/dev/null; then
+        echo "Found bad commit ${BAD_COMMIT}, reverting..."
+        git revert --no-edit ${BAD_COMMIT}
+    else
+        echo "Commit ${BAD_COMMIT} not found, skip revert"
+    fi
+
+    cd -
+else
+    echo "microsocks git repo not found, skip revert"
+fi

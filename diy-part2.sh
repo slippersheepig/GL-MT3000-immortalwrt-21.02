67d45b9e8cf11faacf090fea096b4c73419beb7f
sheepgreen <58287293+slippersheepig@users.noreply.github.com>
2025-12-25 10:10:15 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,6 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
+
+# Fix microsocks build error (Change c99 to gnu99 for gcc 8.4 compatibility)
+sed -i '/include \$(INCLUDE_DIR)\/package.mk/a TARGET_CFLAGS += -fpermissive -std=gnu99' package/luci-app-passwall-packages/microsocks/Makefile
